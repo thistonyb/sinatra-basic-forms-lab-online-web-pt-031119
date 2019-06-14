4936f65300932f8ebccb3b5049a2752640c8a0ee
Tony Baker <this.tonybaker@gmail.com>
2019-06-14 01:13:18 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,7 +1,7 @@
 class Puppy
-  attr_accessor :name, :breed, :age
+  attr_accessor :name, :breed, :months_old
 
-  def initialize(name, breed, age)
+  def initialize(name, breed, months_old)
     @name = name
     @breed = breed
     @months_old = age
