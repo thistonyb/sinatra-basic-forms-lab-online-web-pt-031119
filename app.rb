bb3ff66458befa758248db3b1217651e767e9777
Tony Baker <this.tonybaker@gmail.com>
2019-06-14 01:41:06 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
 
+    erb :index
+  end
+
+  get '/new' do
+
+    erb :create_puppy
+  end
+
+  post '/new' do
+    @name = params[:name]
+    @breed = params[:breed]
+    @months_old = params[:months_old]
+
+    erb :display_puppy
+  end
 end
