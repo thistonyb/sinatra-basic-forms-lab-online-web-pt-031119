3e11126557e47457fad244bb43066432733313f4
Tony Baker <this.tonybaker@gmail.com>
2019-06-14 01:22:22 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,12 +6,12 @@ class App < Sinatra::Base
     erb :index
   end
 
-  get '/create_puppy' do
+  get '/new' do
 
     erb :create_puppy
   end
 
-  post '/create_puppy' do
+  post 'new' do
     @name = params[:name]
     @breed = params[:breed]
     @months_old = params[:months_old]
